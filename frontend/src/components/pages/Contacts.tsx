9d62067294ec44cbdd1c2ef35d131e791b993a58
Miracle <rjcvjchzljv9@gmail.com>
2022-09-14 00:50:37 +0300

    fix Contacts

--- frontend/src/components/pages/Contacts.tsx
+++ frontend/src/components/pages/Contacts.tsx
@@ -1,7 +1,34 @@
+import { Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import React from "react";
+
 export default function Contacts() {
+    const phone = '+888 888 88 88'
     return (
-        <div>
+        <Box>
             <h2>Contacts</h2>
-        </div>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <ListItem>
+                    <ListItemAvatar>
+                        Avatar
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="title"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    phone:&nbsp;
+                                </Typography>
+                                {`${phone}`}
+                            </React.Fragment>
+                        }>
+                    </ListItemText>
+                </ListItem>
+            </List>
+        </Box>
     )
 }
